0494926586759174bb71ed20068acb806753bf02
denibudeyko <denbudeyko@gmail.com>
2020-09-02 18:25:47 +0300

    add filter to price

--- static/js/main.js
+++ static/js/main.js
@@ -21,60 +21,96 @@ var allResultJson = JSON.parse(newResponse)
 var allTickets = allResultJson.tickets
 allTickets = allTickets.slice(0,5)
 
+const homeItems = document.querySelector('.home-items')
+
+
+
+
+function createElements(){
+  
+  allTickets.forEach(item =>{
+
+    var priceItem = item.price;
+    var carrier = item.carrier;
+    var carrierURL = 'pics.avs.io/99/36/' + carrier + '.png'
+    var segments = item.segments;
+    var dateTest = segments[0].date
+
+    
+    const homeItem = document.createElement('div')
+    homeItem.className = 'home-item'
+    
+    homeItem.innerHTML = `
+      <div class="home-item__top">
+        <div class="home-item__price">${priceItem} Р</div>
+        <div class="home-item__logo">
+          <img src="https://${carrierURL}" alt="">
+        </div>
+      </div>
+      <div class="home-item__points">
+        ${segments.map((item) => `
+        
+        <div class="home-point">
+          <ul>
+            <li><span>${item.origin} – ${item.destination}</span><span>${new Date(item.date).toLocaleTimeString("ru", {hour: '2-digit', minute:'2-digit'})} – 08:00</span>
+            </li>
+            <li><span>В пути</span><span>${getTimeFromMins(item.duration)}</span>
+            </li>
+            <li>
+              <span>1 пересадка</span>
+              <span>HKG</span>
+            </li>
+          </ul>
+        </div>
+        `).join('')}
+      </div>
+    `;
+
+    homeItems.appendChild(homeItem);
+    
+  })
+
+}
+
+createElements()
 function getTimeFromMins(mins) {
   let hours = Math.trunc(mins/60);
   let minutes = mins % 60;
   return hours + 'ч. ' + minutes + 'м.';
 };
 
-const homeItems = document.querySelector('.home-items')
-
-
-allTickets.forEach(item =>{
-
-  var priceItem = item.price;
-  var carrier = item.carrier;
-  var carrierURL = 'pics.avs.io/99/36/' + carrier + '.png'
-  var segments = item.segments;
-  var dateTest = segments[0].date
-  console.log(dateTest)
-
-  
-  const homeItem = document.createElement('div')
-  homeItem.className = 'home-item'
-  
-  homeItem.innerHTML = `
-    <div class="home-item__top">
-      <div class="home-item__price">${priceItem} Р</div>
-      <div class="home-item__logo">
-        <img src="https://${carrierURL}" alt="">
-      </div>
-    </div>
-    <div class="home-item__points">
-      ${segments.map((item) => `
-      
-      <div class="home-point">
-        <ul>
-          <li><span>${item.origin} – ${item.destination}</span><span>10:45 – 08:00</span>
-          </li>
-          <li><span>В пути</span><span>${getTimeFromMins(item.duration)}</span>
-          </li>
-          <li>
-            <span>1 пересадка</span>
-            <span>HKG</span>
-          </li>
-        </ul>
-      </div>
-      `).join('')}
-    </div>
-  `;
 
 
 
-  homeItems.appendChild(homeItem);
+const tabsItems = document.querySelectorAll('.home-tabs li')
 
 
-  
+tabsItems.forEach(item => {
+
+  item.addEventListener('click',function(event) {
+
+    tabsItems.forEach(tab => {
+
+      tab.classList.remove('active')
+
+    })
+
+    item.classList.add('active')
+
+    allTickets.sort(function(a, b){
+      if (a.price > b.price) {
+        return 1;
+      }
+      if (a.price < b.price) {
+        return -1;
+      }
+      // a должно быть равным b
+      return 0;
+    })
+    homeItems.innerHTML = '';
+    createElements()
+  })
+
 })
-//pics.avs.io/99/36/{IATA_CODE_HERE}.png
+
 
